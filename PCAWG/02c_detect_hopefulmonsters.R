ff708eb1a77bcc5e2e55862c26f022aa5d225383
jovoni <santacatterinagiovanni@gmail.com>
2025-02-10 19:50:42 +0100

    Mc

--- PCAWG/02c_detect_hopefulmonsters.R
+++ PCAWG/02c_detect_hopefulmonsters.R
@@ -55,7 +55,7 @@ df = RES %>%
   dplyr::distinct() %>% 
   dplyr::mutate(class = ifelse(n_chr > N_CHR, "HM", "Classic")) 
   
-df %>% 
+p = df %>% 
   ggplot(mapping = aes(x=n_events, y=n_clusters, col=class)) +
   geom_point() +
   facet_wrap(~ttype) +
@@ -64,9 +64,10 @@ df %>%
   labs(x = "N CNA", y= "N clusters", col="") +
   scale_x_continuous(breaks = scales::pretty_breaks(n=3)) +
   scale_y_continuous(breaks = scales::pretty_breaks(n=3))
-ggsave("plot/scatter_Ncna_v_Nclusters_with_HM.pdf", width = 8, height = 8, units = "in", plot = last_plot())
+saveRDS(p, "plot/scatter_Ncna_v_Nclusters_with_HM.rds")
+ggsave("plot/scatter_Ncna_v_Nclusters_with_HM.pdf", width = 8, height = 8, units = "in", plot = p)
 
-df %>% 
+p = df %>% 
   dplyr::group_by(ttype, class) %>%
   dplyr::summarise(n = n()) %>%
   dplyr::ungroup() %>%
@@ -81,9 +82,10 @@ df %>%
   scale_y_continuous(limits = c(0,1)) +
   labs(x = "Tumour type", y="HM fraction") +
   theme(legend.position = "none")
-ggsave("plot/distribution_of_HM_fraction_per_ttype.pdf", width = 8, height = 8, units = "in", plot = last_plot())
+saveRDS(p, "plot/distribution_of_HM_fraction_per_ttype.rds")
+ggsave("plot/distribution_of_HM_fraction_per_ttype.pdf", width = 8, height = 8, units = "in", plot = p)
 
-RES %>% 
+p = RES %>% 
   dplyr::group_by(sample_id) %>% 
   dplyr::group_by(ttype, sample_id) %>% 
   dplyr::mutate(n_events=n(), n_clusters=length(unique(clock_mean))) %>% 
@@ -91,7 +93,19 @@ RES %>%
   dplyr::ungroup() %>% 
   dplyr::select(sample_id, ttype, n_events, n_clusters, n_chr) %>% 
   dplyr::distinct() %>% 
-  dplyr::mutate(class = ifelse(n_chr > N_CHR, "HM", "Classic"))
+  dplyr::mutate(class = ifelse(n_chr > N_CHR, "HM", "Classic")) %>% 
+  ggplot(mapping = aes(x=n_events, y=n_clusters, fill=class, col=class)) +
+  stat_density_2d(geom = "polygon", aes(alpha = ..level..)) +
+  geom_point() +
+  scale_x_continuous(transform = "log10") +
+  scale_y_continuous(transform = "log10") +
+  scale_color_manual(values = class_colors) +
+  scale_fill_manual(values = class_colors) +
+  theme_bw()
+p
+
+saveRDS(p, "plot/density_HM_dist.rds")
+ggsave("plot/density_HM_dist.pdf", width = 8, height = 8, units = "in", plot = p)
 
 # q9 = data %>% 
 #   dplyr::mutate(x = n_events / n_clusters) %>% 
