e0a75f7a447524818ee3bacef00d556a343a87b6
saracoco <sara.cocomello@gmail.com>
2025-02-27 16:14:34 +0100

    add job parallel

---
+++ PCAWG/Job_array_PCAWG.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=PCAWG_timing
+#SBATCH --partition=THIN
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=24 
+#SBATCH --mem=50gb
+#SBATCH --time=24:00:00
+#SBATCH --array=1-148
+#SBATCH --output=log/output_%A_%a.out
+#SBATCH --error=log/error_%A_%a.err
+
+module load R/4.4.1
+
+Rscript 04_retrieve_results.R ${SLURM_ARRAY_TASK_ID} 118  
