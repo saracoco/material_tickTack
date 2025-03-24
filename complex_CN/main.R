b50dc23babdda43be8f5cd0c25c413889898caab
saracoco <sara.cocomello@gmail.com>
2025-03-24 12:18:50 +0100

    modify

--- complex_CN/main.R
+++ complex_CN/main.R
@@ -6,7 +6,6 @@ source("./simulate_functions.R")
 x <- readRDS("/orfeo/cephfs/scratch/cdslab/scocomello/data/clonal_analysis_PCAWG/0009b464-b376-4fbc-8a56-da538269a02f/fit.rds")
 x$mutations <- x$snvs
 
-
 # understand from subclonal analysis how to generate evolving data for the clonal case 
 min_VAF = 0
 n_min = 10
@@ -32,20 +31,23 @@ karyotype <- paste(Major, minor, sep=':')
 karyotype_1 = karyotype
 karyotype_2 = karyotype
 
-
 #branching evolution
 #input
 starting = starting_state
-
 
 left = karyotype
 right = karyotype
 target=starting
 
-
-
 b_model = suppressWarnings(branching_evolution(starting, karyotype_1, karyotype_2, CCF_1, purity)) %>%
   Reduce(f = bind_rows)
+
+
+
+
+first_child = karyotype_1
+second_child = karyotype_2
+
 
 
 l1_model = NULL
@@ -53,8 +55,68 @@ if((grepl("0", karyotype_1) & grepl("0", karyotype_2)) | !grepl("0", karyotype_1
   l1_model = suppressWarnings(linear_evolution(starting, karyotype_1, karyotype_2, CCF_1, purity)) %>%
   Reduce(f = bind_rows)
 
+# error in evolve 
+#inputs
+copy_state = initial_state(starting)
+target = first_child
 
 
+
+cap = (target %>% strsplit(split = ':'))[[1]] %>% as.numeric %>% sum
+cap = 2 * cap
+copy_state_tmp = copy_state
+copy_state = copy_state_tmp
+current_state = original_state = list(copy_state)
+current_state_tmp = current_state
+copy_state = current_state
+
+
+mutations = copy_state[[1]] %>% pull(mutations) %>% unlist()
+mutations = copy_state[[1]] %>% filter(allele == !!"A1") %>% pull(mutations) %>% unlist()
+
+
+
+get_mutations = function(copy_state, allele = NULL){
+  if(is.null(allele))
+    mutations = copy_state %>% pull(mutations) %>% unlist()
+  else
+    mutations = copy_state %>% filter(allele == !!allele) %>% pull(mutations) %>% unlist()
+  
+  return(mutations)
+}
+
+
+# Amplify allele
+amplify =  function(copy_state){
+  augment = function(which_allele)
+  {
+    mutations_allele = copy_state[[1]] %>% get_mutations(allele = which_allele)
+    
+    maj_min = substr(which_allele, 0, 1)
+    n_allele = gsub(x = which_allele, 'A', "") %>% gsub(pattern = 'B', replacement = "") %>%
+      as.numeric()
+    
+    # New allele can be +1, unless there are other alleles with larger number
+    new_allele = paste(maj_min, n_allele + 1, sep = '')
+    
+    while(new_allele %in% (copy_state %>% get_alleles())) {
+      n_allele = n_allele + 1
+      new_allele = paste(maj_min, n_allele + 1, sep = '')
+    }
+    
+    new_entry = copy_state[[1]] %>% filter(allele == which_allele)
+    new_entry$allele = new_allele
+    
+    copy_state %>% bind_rows(new_entry) %>% dplyr::arrange(allele)
+  }
+  
+  copy_state %>%
+    get_alleles() %>%
+    lapply(augment)
+}
+
+lapply(current_state, amplify)
+del_new_state = lapply(current_state, delete)
 
 # expected_peaks = lapply(analysis,
 #                         function(k)
