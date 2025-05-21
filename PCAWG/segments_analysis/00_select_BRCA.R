1d3dbbe9f43cc4f0d10cc043c72477467115f274
saracoco <sara.cocomello@gmail.com>
2025-05-21 11:17:42 +0200

    update segment analysis

--- PCAWG/segments_analysis/00_select_BRCA.R
+++
@@ -1,4 +0,0 @@
-res <- readRDS("/orfeo/cephfs/scratch/cdslab/scocomello/material_tickTack/PCAWG/segments_analysis/data/info_segments_1e+06.rds")
-unique(res$type)
-res_BRCA <- res %>% filter(type=="BRCA")
-saveRDS(res_BRCA, "./data/info_segments_BRCA_1e6.rds")
